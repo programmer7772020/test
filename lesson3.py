d1d7cd9069c6bd05327caccfd168c1f8102e6ec6
segoodcoder <kiselev.ga@edu.spbstu.ru>
2020-09-19 15:08:45 +0300

    add file lesson3.py

---
+++ lesson3.py
@@ -0,0 +1,15 @@
+def lcm(m, n):
+    if m > n:
+        mx = m
+        mn = n
+    else:
+        mx = n
+        mn = m
+    while (mx % mn) != 0:
+        mn1 = mn
+        mn = mx % mn
+        mx = mn1
+    k = m * n / mn
+    return k
+
+print(lcm(16,24))
